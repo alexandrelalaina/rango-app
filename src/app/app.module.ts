85d9b7ab492098ea628ff6450ea71523ee075908
Alexandre La Laina <alexandre_lalaina@yahoo.com.br>
2022-10-26 14:08:40 -0300

    primeng add 1 table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,29 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ItensComponent } from './itens/itens.component';
 import { ItemModalComponent } from './modals/item-modal/item-modal.component';
 
+import {TableModule} from 'primeng/table';
+import {ToastModule} from 'primeng/toast';
+import {CalendarModule} from 'primeng/calendar';
+import {SliderModule} from 'primeng/slider';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {ContextMenuModule} from 'primeng/contextmenu';
+import {DialogModule} from 'primeng/dialog';
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {InputTextModule} from 'primeng/inputtext';
+import {FileUploadModule} from 'primeng/fileupload';
+import {ToolbarModule} from 'primeng/toolbar';
+import {RatingModule} from 'primeng/rating';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {InputNumberModule} from 'primeng/inputnumber';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,9 +41,32 @@ import { ItemModalComponent } from './modals/item-modal/item-modal.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TableModule,
+    CalendarModule,
+		SliderModule,
+		DialogModule,
+		MultiSelectModule,
+		ContextMenuModule,
+		DropdownModule,
+		ButtonModule,
+		ToastModule,
+    InputTextModule,
+    ProgressBarModule,
+    HttpClientModule,
+    FileUploadModule,
+    ToolbarModule,
+    RatingModule,
+    FormsModule,
+    RadioButtonModule,
+    InputNumberModule,
+    ConfirmDialogModule,
+    InputTextareaModule
+    ],
+  providers: [
+    MessageService,
+    ConfirmationService
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/itens/itens.component.ts
+++ src/app/itens/itens.component.ts
@@ -1,5 +1,6 @@
 import { ItemModalComponent } from './../modals/item-modal/item-modal.component';
 import { Component, OnInit } from '@angular/core';
+import { MessageService, ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-itens',
@@ -7,18 +8,104 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./itens.component.scss'],
 })
 export class ItensComponent implements OnInit {
-  constructor() {}
 
-  itens = [
-    { id: 1, descricao: 'Calabresa', possuiEstoque: 1 },
-    { id: 2, descricao: 'Frango', possuiEstoque: 1 },
-    { id: 3, descricao: 'Queijo', possuiEstoque: 1 },
-  ];
+    productDialog!: boolean;
 
-  displayedColumns: string[] = [ 'id', 'descricao', 'possuiEstoque' ];
+    products!: any[];
 
-  ngOnInit() {}
+    product: any;
 
-  cadastrarItem() {
-  }
+    selectedProducts: any[] = [];
+
+    submitted!: boolean;
+
+    constructor(private messageService: MessageService, private confirmationService: ConfirmationService) { }
+
+    ngOnInit() {
+        this.products = [ {id : 1, descricao : "Calabresa", possuiEstoque : 1 } ]
+    }
+
+    openNew() {
+        this.product = {};
+        this.submitted = false;
+        this.productDialog = true;
+    }
+
+    deleteSelectedProducts() {
+        this.confirmationService.confirm({
+            message: 'Are you sure you want to delete the selected products?',
+            header: 'Confirm',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                this.products = this.products.filter(val => !this.selectedProducts.includes(val));
+                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Products Deleted', life: 3000});
+            }
+        });
+    }
+
+    editProduct(product: any) {
+        this.product = {...product};
+        this.productDialog = true;
+    }
+
+    deleteProduct(product: any) {
+        this.confirmationService.confirm({
+            message: 'Are you sure you want to delete ' + product.name + '?',
+            header: 'Confirm',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                this.products = this.products.filter(val => val.id !== product.id);
+                this.product = {};
+                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Deleted', life: 3000});
+            }
+        });
+    }
+
+    hideDialog() {
+        this.productDialog = false;
+        this.submitted = false;
+    }
+
+    saveProduct() {
+        this.submitted = true;
+
+        if (this.product.name.trim()) {
+            if (this.product.id) {
+                this.products[this.findIndexById(this.product.id)] = this.product;
+                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Updated', life: 3000});
+            }
+            else {
+                this.product.id = this.createId();
+                this.product.image = 'product-placeholder.svg';
+                this.products.push(this.product);
+                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Created', life: 3000});
+            }
+
+            this.products = [...this.products];
+            this.productDialog = false;
+            this.product = {};
+        }
+    }
+
+    findIndexById(id: string): number {
+        let index = -1;
+        for (let i = 0; i < this.products.length; i++) {
+            if (this.products[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    createId(): string {
+        let id = '';
+        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for ( var i = 0; i < 5; i++ ) {
+            id += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return id;
+    }
+
 }
